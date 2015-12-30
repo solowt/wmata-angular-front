5b6f81bb34363600f8b59672f79e9c983b09b630
Thomas Solow <thomassolow@gmail.com>
2015-12-30 14:00:41 -0500

    writing blog now

--- js/trains/time.directive.js
+++ js/trains/time.directive.js
@@ -23,37 +23,32 @@
           scope.vm.setStation(trainsArray);
         }
         scope.getTrains = function(){
-          if (!scope.lastUpdated || (new Date().getTime() - scope.lastUpdated.getTime())/1000>20) {
-            scope.times = []
-            console.log("Getting data...")
-            scope.lastUpdated = new Date();
-            $.ajax({
-                  url: "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/"+scope.code+"?api_key=635e0ed2348f420cbe874f1bcd5d1b11",
-                  type: "GET",
-                  // Request body
-                  data: "{body}",
-                  dataType: "jsonp"
-              })
-              .done(function(data) {
-                  for (var i=0; i<data["Trains"].length; i++){
-                    var train = {
-                                  cars:data["Trains"][i].Car,
-                                  dest:data["Trains"][i].Destination,
-                                  status:data["Trains"][i].Min,
-                                  locationName:data["Trains"][i].LocationName
-                                }
-                    scope.times.push(train)
-                  }
-                  scope.updateStation(scope.times)
-                  $state.go('getStations')
-
-              })
-              .fail(function() {
-                  console.log("Failed Look-up.")
-              });
-          } else {
-            console.log("Please wait 20 seconds in between refreshes...")
-          }
+          scope.times = []
+          console.log("Getting data...")
+          scope.lastUpdated = new Date();
+          $.ajax({
+                url: "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/"+scope.code+"?api_key=635e0ed2348f420cbe874f1bcd5d1b11",
+                type: "GET",
+                // Request body
+                data: "{body}",
+                dataType: "jsonp"
+            })
+            .done(function(data) {
+              for (var i=0; i<data["Trains"].length; i++){
+                var train = {
+                            cars:data["Trains"][i].Car,
+                            dest:data["Trains"][i].Destination,
+                            status:data["Trains"][i].Min,
+                            locationName:data["Trains"][i].LocationName
+                            }
+                scope.times.push(train)
+              }
+              scope.updateStation(scope.times)
+              $state.go('getStations')
+            })
+            .fail(function() {
+              console.log("Failed Look-up.")
+          });
         }
       }
     }
